e87f6994ff54fd7a0a16ecbeca0c1084f3ebfefc
naufaldymahas <naufaldymahas@gmail.com>
2024-05-23 22:17:29 +0700

    feat(api): add gorm and godotenv

---
+++ api/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() (*gorm.DB, error) {
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		host,
+		user,
+		password,
+		dbName,
+		port,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	return db, err
+}
--- api/main.go
+++ api/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
