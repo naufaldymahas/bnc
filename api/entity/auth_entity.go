ab270eef55bb37f5dc8e6ed6f74fc2a9c224a92a
naufaldymahas <naufaldymahas@gmail.com>
2024-05-23 21:07:00 +0700

    feat(api): add auth login entity

--- api/entity/auth_entity.go
+++ api/entity/auth_entity.go
@@ -19,3 +19,12 @@ func (u *AuthOTP) BeforeCreate(tx *gorm.DB) error {
 	u.ID = ulid.Make().String()
 	return nil
 }
+
+type AuthUser struct {
+	ID          string    `json:"id" gorm:"primaryKey,not null"`
+	UserID      string    `json:"userId" gorm:"not null"`
+	AccessToken string    `json:"accessToken" gorm:"not null"`
+	LastLoginAt time.Time `json:"lastLoginAt" gorm:"not null"`
+	CreatedAt   time.Time `json:"createdAt" gorm:"not null"`
+	UpdatedAt   time.Time `json:"updatedAt" gorm:"not null"`
+}
