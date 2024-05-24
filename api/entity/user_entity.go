5f8db8d3f381bc2e6a72fa1b0e464ebfd91f30ee
naufaldymahas <naufaldymahas@gmail.com>
2024-05-24 13:54:22 +0700

    feat(api): change from unique to uniqueIndex for email and phone number

--- api/entity/user_entity.go
+++ api/entity/user_entity.go
@@ -10,8 +10,8 @@ type User struct {
 	ID                     string            `json:"id" gorm:"primaryKey,not null"`
 	Name                   string            `json:"name" gorm:"not null"`
 	Role                   constant.UserRole `json:"role" gorm:"not null"`
-	PhoneNumber            string            `json:"phoneNumber" gorm:"unique,not null"`
-	Email                  string            `json:"email" gorm:"unique,not null"`
+	PhoneNumber            string            `json:"phoneNumber" gorm:"uniqueIndex,not null"`
+	Email                  string            `json:"email" gorm:"uniqueIndex,not null"`
 	Password               string            `json:"password" gorm:"not null"`
 	CorporateAccountNumber string            `json:"corporateAccountNumber" gorm:"not null"`
 	CreatedAt              time.Time         `json:"createdAt" gorm:"not null"`
