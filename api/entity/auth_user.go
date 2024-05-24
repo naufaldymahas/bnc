2b38d175e4f6c17110f2ed8518d01ea116ca524d
naufaldymahas <naufaldymahas@gmail.com>
2024-05-24 10:33:41 +0700

    feat(api): seperate auth entity files

--- api/entity/auth_user.go
+++ api/entity/auth_user.go
@@ -7,19 +7,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type AuthOTP struct {
-	ID        string    `json:"id" gorm:"primaryKey,not null"`
-	OTP       string    `json:"otp" gorm:"not null"`
-	Email     string    `json:"email" gorm:"not null"`
-	CreatedAt time.Time `json:"createdAt" gorm:"not null"`
-	UpdatedAt time.Time `json:"updatedAt" gorm:"not null"`
-}
-
-func (u *AuthOTP) BeforeCreate(tx *gorm.DB) error {
-	u.ID = ulid.Make().String()
-	return nil
-}
-
 type AuthUser struct {
 	ID          string    `json:"id" gorm:"primaryKey,not null"`
 	UserID      string    `json:"userId" gorm:"not null"`
@@ -27,4 +14,9 @@ type AuthUser struct {
 	LastLoginAt time.Time `json:"lastLoginAt" gorm:"not null"`
 	CreatedAt   time.Time `json:"createdAt" gorm:"not null"`
 	UpdatedAt   time.Time `json:"updatedAt" gorm:"not null"`
+}
+
+func (u *AuthUser) BeforeCreate(tx *gorm.DB) error {
+	u.ID = ulid.Make().String()
+	return nil
 }
