f16768737654ff1933af76e7133c22791baf5eca
naufaldymahas <naufaldymahas@gmail.com>
2024-05-24 21:41:58 +0700

    feat(web): add form shadcn to register page

--- web/src/app/register/page.tsx
+++ web/src/app/register/page.tsx
@@ -1,53 +1,187 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { RegisterSchema, UserRole } from "@/lib/schema/auth";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-export interface RegisterProps {}
+export default function Register() {
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      corporateAccountNumber: "",
+      corporateName: "",
+      otp: "",
+      password: "",
+      userEmail: "",
+      userId: "",
+      userName: "",
+      userPhoneNumber: "",
+    },
+  });
 
-export default function Register(props: RegisterProps) {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    console.log(values);
+  };
+
   return (
-    <div className="container h-screen">
-      <div className="flex flex-col justify-center mx-auto h-full md:w-1/3">
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="corporateAccountNo">Corporate Account No.</Label>
-          <Input id="corporateAccountNo" placeholder="Corporate Account No." />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="corporateName">Corporate Name</Label>
-          <Input id="corporateName" placeholder="Corporate Name" />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="userId">User ID</Label>
-          <Input id="userId" placeholder="User ID" />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="userName">User Name</Label>
-          <Input id="userName" placeholder="User Name" />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="role">Role</Label>
-          <Input id="role" placeholder="Role" />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="phoneNo">Phone No.</Label>
-          <Input id="phoneNo" placeholder="Phone No." />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="email">Email</Label>
-          <Input type="email" id="email" placeholder="Email" />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="password">Password</Label>
-          <Input type="password" id="password" placeholder="Password" />
-        </div>
-        <div className="grid w-full items-center gap-1.5 mb-3">
-          <Label htmlFor="verificationCode">Verification Code</Label>
-          <Input id="verificationCode" placeholder="Verification Code" />
-        </div>
-        <Button variant={"yellow"} className="mb-3" disabled={true}>
-          Submit
-        </Button>
+    <div className="container">
+      <div className="flex flex-col justify-center mx-auto min-h-screen md:w-1/3">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="corporateAccountNumber"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Corporate Account No.</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Corporate Account No." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="corporateName"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Corporate Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Corporate Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userId"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>User ID</FormLabel>
+                  <FormControl>
+                    <Input placeholder="User ID" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userName"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>User Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="User Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userRole"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Role</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Role" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value={UserRole.Maker}>Maker</SelectItem>
+                      <SelectItem value={UserRole.Approver}>
+                        Approver
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userPhoneNumber"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Phone No.</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Phone No." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="userEmail"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="Email" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="otp"
+              render={({ field }) => (
+                <FormItem className="mb-3">
+                  <FormLabel>Verification Code</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Verification Code" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button variant={"yellow"} className="mb-3 w-full" type="submit">
+              Submit
+            </Button>
+          </form>
+        </Form>
         <p>
           Already have an account?{" "}
           <Link
--- web/src/lib/schema/auth.ts
+++ web/src/lib/schema/auth.ts
@@ -5,3 +5,20 @@ export const LoginSchema = z.object({
   userId: z.string().min(1),
   password: z.string().min(1),
 });
+
+export enum UserRole {
+  Maker = "maker",
+  Approver = "approver",
+}
+
+export const RegisterSchema = z.object({
+  corporateAccountNumber: z.string().min(1),
+  corporateName: z.string().min(1),
+  userId: z.string().min(1),
+  userName: z.string().min(1),
+  userRole: z.nativeEnum(UserRole),
+  userPhoneNumber: z.string().min(1),
+  userEmail: z.string().email(),
+  password: z.string().min(1),
+  otp: z.string().min(1),
+});
