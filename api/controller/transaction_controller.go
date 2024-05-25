f3694a57b6f0aa93ea55346309c2e97bf03c3a5b
naufaldymahas <naufaldymahas@gmail.com>
2024-05-25 20:15:00 +0700

    feat(api): add upload file transfer

--- api/controller/transaction_controller.go
+++ api/controller/transaction_controller.go
@@ -1,7 +1,9 @@
 package controller
 
 import (
+	"encoding/csv"
 	"log"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,9 +15,35 @@ func NewTransactionController(e *echo.Echo) {
 	ctr := TransactionController{}
 
 	e.GET("/v1/transaction/transfer-template", ctr.TransferTemplate)
+	e.POST("/v1/transaction/upload", ctr.UploadTransaction)
 }
 
 func (ctr *TransactionController) TransferTemplate(c echo.Context) error {
 	log.Println("masuk?")
 	return c.Attachment("assets/transfer_template.csv", "transfer_template.csv")
+}
+
+func (ctr *TransactionController) UploadTransaction(c echo.Context) error {
+	file, err := c.FormFile("file")
+
+	f, err := file.Open()
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = -1 // Allow variable number of fields
+	data, err := reader.ReadAll()
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	// Print the CSV data
+	for _, row := range data {
+		for _, col := range row {
+			log.Printf("%s,", col)
+		}
+		log.Println()
+	}
+
+	return c.String(http.StatusOK, "ok")
 }
