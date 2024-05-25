786dcf2acf03bc65ebbed0fc0c3811a2d9bcf210
naufaldymahas <naufaldymahas@gmail.com>
2024-05-25 20:17:02 +0700

    feat(web): init page transaction

---
+++ web/src/app/(home)/transaction/page.tsx
@@ -0,0 +1,11 @@
+import { DashboardCard } from "@/components/dashboard/card";
+
+export default function Transaction() {
+  return (
+    <>
+      <DashboardCard className="mb-3">
+        <h3 className="text-lg font-semibold">Transaction List</h3>
+      </DashboardCard>
+    </>
+  );
+}
